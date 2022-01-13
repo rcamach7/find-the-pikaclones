4dc412b1a3d38b4cbbfe0a477def4281fab681e7
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-01-12 16:15:24 -0800

    linked succesfully to firebase and submits user results upon game completion

--- src/components/EndGame.js
+++ src/components/EndGame.js
@@ -1,11 +1,41 @@
+import { useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
+
 export default function EndGame(props) {
+  const pushToDatabase = async () => {
+    try {
+      await setDoc(doc(getFirestore(), "records", uuidv4()), {
+        userName: props.userName,
+        userTime: props.userTime * -1,
+      });
+    } catch (error) {
+      console.error("Error writing to database", error);
+    }
+  };
+
+  const submitResult = (e) => {
+    // Process user data and prevent page refreshing
+    e.preventDefault();
+    pushToDatabase();
+
+    alert("Result Sent To DB");
+  };
+
   return (
     <div className="EndGame">
       <h2>Congratulations!</h2>
       <h3>You have caught em' all!</h3> <br />
       <p>Username: {props.userName}</p>
       <p>Time: {props.userTime * -1} seconds</p> <br />
-      <button className="endGame-btn">Submit Record</button>
+      <button
+        onClick={(e) => {
+          submitResult(e);
+        }}
+        className="endGame-btn"
+      >
+        Submit Record
+      </button>
     </div>
   );
 }
